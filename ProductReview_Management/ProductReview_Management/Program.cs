cecdc67429b842cc13f11e93048e4a39240d79b9
Priyanka-pardeshi <priyankasspardeshi@gmail.com>
2021-07-21 00:54:24 +0530

    Welcome to product review

---
+++ ProductReview_Management/ProductReview_Management/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProductReview_Management
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to product review management system");
+        }
+    }
+}
