05f7de948d977c0db287e429460a7bbff9627df9
Priyanka-pardeshi <priyankasspardeshi@gmail.com>
2021-07-21 21:12:59 +0530

    Setting and getting reviews for product

--- ProductReview_Management/ProductReview_Management/Program.cs
+++ ProductReview_Management/ProductReview_Management/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ProductReview_Management
 {
@@ -7,6 +8,36 @@ namespace ProductReview_Management
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to product review management system");
+
+            List<Review_product> review = new List<Review_product>()
+            {
+                new Review_product() {ProductId = 1, UserId = 1, Rating = 5, Review = "Good", Like = false},
+                new Review_product() {ProductId = 2, UserId = 2, Rating = 5, Review= "Good", Like = false},
+                new Review_product() {ProductId = 3, UserId = 3, Rating = 4, Review = "Good", Like = true},
+                new Review_product() {ProductId = 4, UserId = 4, Rating = 3, Review = "Bad", Like = true},
+                new Review_product() {ProductId = 5, UserId = 5, Rating = 2, Review = "Bad", Like = true},
+                new Review_product() {ProductId = 6, UserId = 6, Rating = 4, Review = "Good", Like = false},
+                new Review_product() {ProductId = 7, UserId = 7, Rating = 3.5, Review = "Good", Like = false},
+                new Review_product() {ProductId = 8, UserId = 8, Rating = 2.5, Review = "Bad", Like = true},
+                new Review_product() {ProductId = 9, UserId = 9, Rating = 5, Review = "Good", Like = true},
+                new Review_product() {ProductId = 10, UserId = 10, Rating = 3.6, Review = "Good", Like = true},
+                new Review_product() {ProductId = 11, UserId = 11, Rating = 4.5, Review = "Good", Like = true},
+                new Review_product() {ProductId = 12, UserId = 12, Rating = 2.8 , Review = "Good", Like = true},
+                new Review_product() {ProductId = 13, UserId = 1, Rating = 1.8, Review = "Bad", Like = false},
+                new Review_product() {ProductId = 14, UserId = 5, Rating = 1, Review = "Bad", Like = false},
+                
+            };
+
+            foreach (var reviewList in review)
+            {
+                Console.WriteLine("Product ID : " + reviewList.ProductId);
+                Console.WriteLine("User ID    : " + reviewList.UserId);
+                Console.WriteLine("Rating     : " + reviewList.Rating);
+                Console.WriteLine("Review     : " + reviewList.Review);
+                Console.WriteLine("Is Like    : " + reviewList.Like + "\n");
+            }
+            Console.ReadKey();
         }
+
     }
 }
---
+++ ProductReview_Management/ProductReview_Management/Review_product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductReview_Management
+{
+    class Review_product
+    {
+        public int ProductId { get; set; }
+        public int UserId { get; set; }
+        public double Rating { get; set; }
+        public string Review { get; set; }
+        public bool Like { get; set; }
+
+    }
+}
